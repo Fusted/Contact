379bc115d7c811ceca5513d5fac49b4c454c89ac
Fusted <tihantsov@gmail.com>
2022-01-01 21:23:36 +0300

    Update isLogined.js

--- src/components/isLogined/isLogined.js
+++ src/components/isLogined/isLogined.js
@@ -1,14 +1,15 @@
-import { Component } from "react";
 import LoginForm from "../loginForm/loginForm";
 import Main from "../main/main";
 
-export default class IsLogined extends Component {
-  render() {
-    if (this.props.authorize === true) {
+function IsLogined(props) {
+  
+    if (props.authorize === true) {
       return <Main />;
     } else {
       return <LoginForm 
-      onCorrectForm={this.props.onCorrectForm} />;
+      onCorrectForm={props.onCorrectForm} />;
     }
-  }
+  
 }
+
+export default IsLogined
